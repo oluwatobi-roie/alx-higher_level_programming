a1c76fc649eb03eee040642b527dd842b0cc588f
Victor Oluwatobi <akomolafevicto@gmail.com>
2022-11-30 09:14:32 -0800

    fixed until question 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 10):
     for j in range(i, 10):
         if i == 8 and j == 9:
-            print(f'{i}{j}')
+            print('{}{}'.format(i, j))
         elif i != j:
-            print(f'{i}{j}, ', end="")
+            print('{}{}, '.format(i, j), end="")
