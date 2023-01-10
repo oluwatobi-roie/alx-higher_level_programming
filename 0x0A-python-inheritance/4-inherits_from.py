61684d7fe42a143eed3e68a74b6a841b474ccdb9
Oluwatobi Victor <68193330+oluwatobi-roie@users.noreply.github.com>
2023-01-10 21:48:28 +0100

    Inherited on 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-''' module: 4-inherits_from
-'''
+"""checks if object is an instance of a class that
+inherited from the specified class or not
+"""
 
 
 def inherits_from(obj, a_class):
-    '''the object is an instance of a class that inherited (directly or indirectly)
-    obj: an object
-    a_class: a class
-    returns None
-    '''
-    return type(obj) != a_class and isinstance(obj, a_class)
+    """Returns true if object is an instance of a class that inherited
+    (directly or indirectly) from the specified class; otherwise False
+    """
+    return (issubclass(type(obj), a_class) and type(obj) != a_class)
